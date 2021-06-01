fcb58fc1cf5e19a8083e46219f9902595ddf981c
zzpz <michael.wilson1@uqconnect.edu.au>
2021-06-01 21:08:44 +1000

    python doc template

---
+++ 0. pydoc template.py
@@ -0,0 +1,19 @@
+class Solution:
+    def method(self, param1: List[int], param2: int) -> Any:
+        """
+        https://google.github.io/styleguide/pyguide.html#383-functions-and-methods
+
+        Description, lorem ipsum.
+
+        Args:
+            param1: This is the first param.
+            param2: This is a second param.
+
+        Returns:
+            This is a description of what is returned.
+
+        Raises:
+            KeyError: Raises an exception.
+        """
+
+        pass
